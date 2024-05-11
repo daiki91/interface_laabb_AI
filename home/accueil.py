dde5ca2afe4c58638618d7c28e66ec907d58336a
CoumbaBKANE <coumbakane41@gmail.com>
2024-05-11 17:10:05 +0000

    commit

--- home/accueil.py
+++ home/accueil.py
@@ -6,7 +6,27 @@ def Accueil():
     def show_model1():
         st.write("Contenu du Modèle 1")
     def show_model_2():
-        st.write("Contenu du Modèle 2")
+        st.subheader("Renseigner les informations")
+        with st.form(key = 'infos_pluie'):
+            annee = st.slider("Annee", 1990, 2030, 2023 )
+            type_sol = st.selectbox("Type de sol", ["Argileux", "Sableux", "Limoneux"])
+            type_culture = st.selectbox("Type de culture", ["Céréales", "Légumes", "Fruits"])
+            prenom = st.text_input("Précipitations moyennes en millimètres par an")
+            pet = st.text_input("Pesticides en tonnes")
+            temp_moy = st.text_input("Temperature moyenne")
+
+            submitted = st.form_submit_button()
+            if submitted:  
+                form_data = {
+                    "annee": annee,
+                    "type_sol": type_sol,
+                    "type_culture": type_culture,
+                    "prenom": prenom,
+                    "pet": pet,
+                    "temp_moy": temp_moy
+                }
+                st.success("formulaire soumis avec succes!")
+                st.write(form_data)
     def show_model_3():
             st.write("Contenu du Modèle 3")
     def show_model_4():
