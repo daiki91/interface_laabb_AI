19c8e411a9437671b4724d6b8155d10e1319c685
CoumbaBKANE <coumbakane41@gmail.com>
2024-05-12 00:21:09 +0000

    commit

--- home/accueil.py
+++ home/accueil.py
@@ -11,8 +11,8 @@ def Accueil():
             annee = st.slider("Annee", 1990, 2030, 2023 )
             type_sol = st.selectbox("Type de sol", ["Argileux", "Sableux", "Limoneux"])
             type_culture = st.selectbox("Type de culture", ["Céréales", "Légumes", "Fruits"])
-            prenom = st.text_input("Précipitations moyennes en millimètres par an")
-            pet = st.text_input("Pesticides en tonnes")
+            precip_moy_an = st.number_input("Précipitations moyennes en millimètres par an")
+            pesti_tonne = st.number_input("Pesticides en tonnes")
             temp_moy = st.text_input("Temperature moyenne")
 
             submitted = st.form_submit_button()
@@ -21,17 +21,29 @@ def Accueil():
                     "annee": annee,
                     "type_sol": type_sol,
                     "type_culture": type_culture,
-                    "prenom": prenom,
-                    "pet": pet,
+                    "precip_moy_an": precip_moy_an,
+                    "pesti_tonne": pesti_tonne,
                     "temp_moy": temp_moy
                 }
                 st.success("formulaire soumis avec succes!")
                 st.write(form_data)
     def show_model_3():
-            st.write("Contenu du Modèle 3")
+        st.write("Contenu du Modèle 3")
     def show_model_4():
-            st.write("Contenu du Modèle 4")
-    selected_model = st.sidebar.radio("", ["Tableau de Bord","Analyse et Prévision", "Estimation des Coûts et Revenus", "Recommandations ", "Risques Climatiques et Agricoles"])
+        st.header("Posez vos questions ici")
+        # st.write("Quel est le risque climatique le plus fréquent?")
+        # st.write("Que faire en cas de sécheresse?")
+        # st.write("Comment prévenir les maladies des plantes?")
+        # st.write("Comment prévenir les attaques des ravageurs?")
+       
+        st.markdown("""***Quel est le risque climatique le plus fréquent?***""")
+        st.markdown("""***Que faire en cas de sécheresse?***""")
+        st.markdown("""***Comment prévenir les maladies des plantes?***""")
+        st.markdown("""***Comment prévenir les attaques des ravageurs?***""")
+            
+    def show_model_5():
+        st.write("Contenu du Modèle 5")
+    selected_model = st.sidebar.radio("", ["Tableau de Bord","Analyse et Prévision", "Estimation des Coûts et Revenus", "Recommandations", "Risques Climatiques et Agricoles"])
 
     if selected_model == "Tableau de Bord":
         show_model1()
@@ -39,6 +51,8 @@ def Accueil():
         show_model_2()
     elif selected_model == "Estimation des Coûts et Revenus":
         show_model_3()
-    elif selected_model == "Risques Climatiques et Agricoles":
+    elif selected_model == "Recommandations":
         show_model_4()
+    elif selected_model == "Risques Climatiques et Agricoles":
+        show_model_5()
 
